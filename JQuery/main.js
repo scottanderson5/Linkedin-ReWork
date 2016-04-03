01f9c9e455d33d77974feb2fdbf13616737cff12
Scott <scottanderson5@gmail.com>
2016-04-03 10:05:22 -0500

    Right Side and Footer Completed

--- JQuery/main.js
+++ JQuery/main.js
@@ -1,1 +1,12 @@
-//document.write('worked');
+
+
+
+	$('#button').click(function(){
+		if ($(this).hasClass('buttonActive')){
+	$(this).removeClass('buttonActive').addClass('buttonNews');
+			}
+			else{
+				$(this).removeClass('buttonNews').addClass('buttonActive');
+			}
+	});
+	
